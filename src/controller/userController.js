d410d236ed6a6b34908c133f5e8d6a18cd7e1398
opeyemibioku <opeyemibioku@gmail.com>
2023-08-02 14:05:03 +0100

    updated the reset password controller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -118,7 +118,7 @@ const userController = {
 
       res.status(200).json({
         message: "OTP sent to email for verification",
-        data: { user: emailExists },
+        data: { user: emailExists, email: email },
       });
     } else {
       res.status(404).json({ message: "User not found", status: "Error" });

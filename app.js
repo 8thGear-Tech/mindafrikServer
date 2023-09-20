9b8f8c0836aeb5da730e9369a42c6179eb53c851
opeyemibioku <opeyemibioku@gmail.com>
2023-09-20 09:16:01 +0100

    resolved cors issue

--- app.js
+++ app.js
@@ -54,34 +54,34 @@ app.use("/user", userRouter);
 app.use("/subscriber", newsletterRouter);
 app.use("/booking", bookSessionRouter);
 
-// app.use(
-//   cors({
-//     origin: "https://www.mindafrik.com",
-//     credentials: true,
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow the necessary HTTP methods
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204, // Set the status code for successful OPTIONS requests
-//     // origin: "http://localhost:4000",
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://www.mindafrik.com",
+    credentials: true,
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow the necessary HTTP methods
+    // preflightContinue: false,
+    optionsSuccessStatus: 200, // Set the status code for successful OPTIONS requests
+    // origin: "http://localhost:4000",
+  })
+);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.mindafrik.com");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://www.mindafrik.com");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Private-Network", true);
+//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//   res.setHeader("Access-Control-Max-Age", 7200);
 
-  next();
-});
+//   next();
+// });
 
 // error handler
 app.use(globalErrorHandler);
